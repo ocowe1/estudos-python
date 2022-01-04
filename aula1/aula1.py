326edf480edd9346c2fc91af0ba2acfbb846b147
Vinicius Costa <rs.vinicius.santos@unimes.br>
2022-01-04 16:49:14 -0300

    aula 2

---
+++ aula1/aula1.py
@@ -0,0 +1,21 @@
+# Aula 1 - Comentários
+
+# Comentário
+print('Linha 1')  # Outro comentário
+print('Linha 2')  # Outro
+
+# Uma linha
+# Outra linha
+
+"""
+print('Linha 3')
+"""
+
+'''
+print('Linha 4')
+'''
+
+print('Linha 5')  # Isso é um comentário
+print('Linha 6')  # Mais um
+print('Linha 7')
+# Final
---
+++ aula2/aula2.py
@@ -0,0 +1,10 @@
+# Aula 2 - Comando print
+
+print(123456)
+print('Vinicius', 'Costa', sep='-', end='.')
+
+"""
+524.684.564-85
+"""
+
+
