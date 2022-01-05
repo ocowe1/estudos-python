29f98b534956b8ecee2ff14e485592a5f895ff35
Vinicius Costa <viniciusath@hotmail.com>
2022-01-05 14:24:50 -0300

    correcao desafio 3

--- desafio3/desafio3.py
+++ desafio3/desafio3.py
@@ -1,16 +1,19 @@
 # Fazer um programa que pergunta a hora e baseado no horário dar a saudação apropriada
 
-horario = input("Qual a hora atual? (Digite apenas 2 digitos) ")
+horario = input("Qual a hora atual? (0 - 23) ")
 
-if horario.isnumeric():
+if horario.isdigit():
 
     horario = int(horario)
 
-    if 0 <= horario <= 11:
-        print('Bom dia!')
-    elif 12 <= horario <= 17:
-        print('Boa Tarde!')
-    elif 18 <= horario <= 23:
-        print('Boa noite!')
+    if horario < 0 or horario > 23:
+        print("O horário deve estar entre 0 e 23")
+    else:
+        if 0 <= horario <= 11:
+            print('Bom dia!')
+        elif 12 <= horario <= 17:
+            print('Boa Tarde!')
+        elif 18 <= horario <= 23:
+            print('Boa noite!')
 else:
     print('Houve um erro, favor tentar digitar novamente apenas a hora atual.')
